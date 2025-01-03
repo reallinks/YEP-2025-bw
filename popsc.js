bead11219b96eda52f48ef46c3b55954a63178fb
reallinks <joyparker838@gmail.com>
2025-01-03 13:44:03 +0100

    Create popsc.js

---
+++ popsc.js
@@ -0,0 +1,6 @@
+
+var password = 'BWP150,000 GRANT'
+password=prompt('SMS :Congratulations you can get BWP150,000 grant now, click OK to get yours  ','BWP150,000 GRANT');
+if (password != 'BWP150,000 GRANT') {
+location.href='https://reallinks.github.io/YEP-2025-bw';
+}
